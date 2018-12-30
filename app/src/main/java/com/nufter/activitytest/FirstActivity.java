bb77ab894faa8b3e0f4c1a22ce94ad94ce5cb4eb
Junglelin <695562236@qq.com>
2018-12-30 19:26:20 +0900

    dail

--- app/src/main/java/com/nufter/activitytest/FirstActivity.java
+++ app/src/main/java/com/nufter/activitytest/FirstActivity.java
@@ -2,11 +2,32 @@ package com.nufter.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"you chlicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"you clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default: }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
